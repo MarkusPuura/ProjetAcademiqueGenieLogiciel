a234ef86b1d95d6e479b8ba34d7115885c11193b
Anna DI PLACIDO <anna.di-placido@etu.unice.fr>
2024-01-09 16:03:12 +0100

    ajout dossier images boutonProjectiles dans images

--- Modele/proj/ButtonsProjectile.java
+++ Modele/proj/ButtonsProjectile.java
@@ -1,4 +1,5 @@
 package Modele.proj;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -7,28 +8,33 @@ public class ButtonsProjectile {
     BufferedImage ameliorateB;
     BufferedImage sellB;
     BufferedImage returnB;
-    public ButtonsProjectile(){
+
+    public ButtonsProjectile() {
         GetImage();
     }
 
-
-
-    public void GetImage(){
-        try{
-            ameliorateB = ImageIO.read(getClass().getResourceAsStream("../../boutonsProjectileSelectionne/BoutonFleche.png"));
-            sellB = ImageIO.read(getClass().getResourceAsStream("../../boutonsProjectileSelectionne/BoutonDollar.png"));
-            returnB = ImageIO.read(getClass().getResourceAsStream("../../boutonsProjectileSelectionne/BoutonSortie.png"));
-        }catch(IOException e){
+    public void GetImage() {
+        try {
+            ameliorateB = ImageIO
+                    .read(getClass().getResourceAsStream("../../images/boutonsProjectileSelectionne/BoutonFleche.png"));
+            sellB = ImageIO
+                    .read(getClass().getResourceAsStream("../../images/boutonsProjectileSelectionne/BoutonDollar.png"));
+            returnB = ImageIO
+                    .read(getClass().getResourceAsStream("../../images/boutonsProjectileSelectionne/BoutonSortie.png"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public BufferedImage getAmeliorateB(){
+
+    public BufferedImage getAmeliorateB() {
         return this.ameliorateB;
     }
-    public BufferedImage getSellB(){
+
+    public BufferedImage getSellB() {
         return this.sellB;
     }
-    public BufferedImage getReturnB(){
+
+    public BufferedImage getReturnB() {
         return this.returnB;
     }
 }
